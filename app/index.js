70ee0e8150325c3bfe562f40c765bf85891ae5ca
JRG_Josh <928139808@qq.com>
2017-03-24 23:24:36 +0800

    显示html页面

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@
  */
 
 const fs = require('fs');
+const path = require('path')
 
 class App {
 	constructor(){
@@ -10,13 +11,27 @@ class App {
 	}
 	initServer(){
 		//初始化的工作
-		let _package = require('../package');
+		
 		return (request,response)=>{
+			let { url } = request;	//==>let url = request.url
+			
 			//每个请求逻辑
-			fs.readFile('./public/index.html','utf8',(error,data)=>{
+			if (url == '/css/index.css'){
+				fs.readFile('./public/css/index.css','utf-8',(err,data)=>{
+					response.end(data)
+				})
+			}
+			if (url == '/js/index.js'){
+				fs.readFile('./public/js/index.js','utf-8',(err,data)=>{
+					response.end(data)
+				})
+			}
+			
+			fs.readFile('./public/index.html',(error,data)=>{
 				// response.end(JSON.stringify(_package))
-				response.end('josh success')
+				response.end(data)
 			})
+			
 		}
 	}
 }
