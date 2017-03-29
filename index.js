65a7cb2c36c03ecc52f91b07b29ed322ac0eb046
JRG_Josh <928139808@qq.com>
2017-03-29 09:46:50 +0800

    step-3

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 // console.log(another);
 
 
-const http = require('http');
-const PORT = 7000;
-const App =require('./app');
-const server = new App();
+const http = require('http');  //HTTP模块
+const PORT = 7000;				//指定端口7000
+const App =require('./app');	//app模块
+const server = new App();		
 http.createServer(server.initServer()).listen(PORT,()=>{
 	console.log(`server listening on port ${PORT}`)
 
